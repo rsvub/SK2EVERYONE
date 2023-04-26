fa857db1b9eac3fb6390883689626895ef3a0f6d
Radek Švub <radek.svub@seznam.cz>
2023-04-26 12:38:15 +0200

    IEnumerable OK

--- DAL/HIHs/HIHSrcDb.cs
+++ DAL/HIHs/HIHSrcDb.cs
@@ -26,7 +26,6 @@ namespace SK2EVERYONE.DAL.HIHs
             cmd.CommandText = "SELECT T.KOC0441, T.NAC044101, T.NAC044102, T3.KOC0440 FROM A00C0441 T LEFT JOIN A00C0440 T3 ON(T3.ICI0000 = T.ODI0440)";
         }
 
-        //TODO: Radek, implement correct IEnumerable
 
         public IEnumerable<HIH> GetAllHIH()
         {
@@ -41,46 +40,7 @@ namespace SK2EVERYONE.DAL.HIHs
                     IdWithoutRegion = rdr[3].ToString()
                 };
             }
-
-
         }
-
-#if false
-        {
-
-
-            List<HIH> hihList = new List<HIH>();
-            try
-            {
-                using (SqlConnection con = new SqlConnection(connectionStringSourceSql))
-                {
-                    SqlCommand cmd = new SqlCommand();
-                    cmd.Connection = con;
-                    cmd.CommandType = System.Data.CommandType.Text;
-                    cmd.CommandText = "SELECT T.KOC0441, T.NAC044101, T.NAC044102, T3.KOC0440 FROM A00C0441 T LEFT JOIN A00C0440 T3 ON(T3.ICI0000 = T.ODI0440)";
-                    con.Open();
-                    SqlDataReader rdr = cmd.ExecuteReader();
-                    while (rdr.Read())
-                    {
-                        hihList.Add(new HIH
-                        {
-                            Id = rdr[0].ToString(),
-                            Name = rdr[1].ToString(),
-                            Region = rdr[2].ToString(),
-                            IdWithoutRegion = rdr[3].ToString()
-                        });
-                    }
-                }
-
-            }
-            catch (InvalidOperationException ex)
-            {
-                Console.WriteLine(ex.GetType().FullName);
-                Console.WriteLine(ex.Message);
-            }
-            return hihList;
-        }
-#endif
     }
 }
 

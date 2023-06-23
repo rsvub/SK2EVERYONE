192e657dbf1307bfea20953ecb70d1a93afd09a7
Radek Švub <radek.svub@seznam.cz>
2023-06-23 14:45:49 +0200

    adding DeliveryCodes

---
+++ Model/DeliveryCode.cs
@@ -0,0 +1,11 @@
+﻿namespace SK2EVERYONE.Model
+{
+    public class DeliveryCode
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string Code { get; set; }
+        public int PartnerId { get; set; }
+        public int IsActive { get; set; }
+    }
+}
